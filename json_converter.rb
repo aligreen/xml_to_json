49fbb5e7f3a3ad781d2c1669dab8d7347afdd772
aligreen <aligreen@ccs.neu.edu>
2013-11-21 15:08:02 -0500

    add user input of files

--- json_converter.rb
+++ json_converter.rb
@@ -2,9 +2,12 @@ require 'active_support/core_ext'
 require 'nokogiri'
 require 'XMLSimple'
 
-doc = Nokogiri::XML(File.read "test.xml")
+doc = Nokogiri::XML(File.read ARGV.first)
 
-doc.search('//property').each do |listing|
+#set listing id
+$node_name = '//property'
+
+doc.search($node_name).each do |listing|
   xml_file = listing.to_xml
   obj = XmlSimple.xml_in(xml_file, :ForceArray => false)
   File.write(obj['details']['provider-listingid'] + '.json', obj.to_json)
