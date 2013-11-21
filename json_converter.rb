eb114c1b92063c34fe8006e7a0616d2d841c0957
aligreen <aligreen@ccs.neu.edu>
2013-11-21 15:04:50 -0500

    refactoring json converter

--- json_converter.rb
+++ json_converter.rb
@@ -4,15 +4,8 @@ require 'XMLSimple'
 
 doc = Nokogiri::XML(File.read "test.xml")
 
-xml_files = []
 doc.search('//property').each do |listing|
-  xml_file = listing.at('provider-listingid').text + '.xml'
-  xml_files << xml_file
-  File.write(xml_file, listing.to_xml)
-end
-
-
-xml_files.each do |file|
-  obj = XmlSimple.xml_in(File.read(file), :ForceArray => false)
+  xml_file = listing.to_xml
+  obj = XmlSimple.xml_in(xml_file, :ForceArray => false)
   File.write(obj['details']['provider-listingid'] + '.json', obj.to_json)
 end
